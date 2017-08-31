8dde8c515b81e89d9487deb560677902a5b17a1c
Ben Crisostomo <beni.blc@gmail.com>
2017-08-31 11:46:47 -0400

    added container connecting redux with react

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [ 
+        {title: 'Hip Hop Moose'},
+        {title: 'Jack Tastic'},
+        {title: 'Pregnant Space'},
+        {title: 'Big Ups part 4'}
+    ]
+}
