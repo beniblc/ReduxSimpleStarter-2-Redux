78bfdda45cc65e3ada7ad93f402d38054280f257
Ben Crisostomo <beni.blc@gmail.com>
2017-10-03 13:36:18 -0400

    learned the how to use containers & actions to link the state to the reducers and modify them.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [ 
-        {title: 'Hip Hop Moose'},
-        {title: 'Jack Tastic'},
-        {title: 'Pregnant Space'},
-        {title: 'Big Ups part 4'}
+        {title: 'Hip Hop Moose', pages: 101},
+        {title: 'Jack Tastic', pages: 43},
+        {title: 'Pregnant Space', pages: 1033},
+        {title: 'Big Ups part 4', pages: 1}
     ]
 }
